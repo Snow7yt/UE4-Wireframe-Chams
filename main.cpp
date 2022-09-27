5d43b29cdd503d850c5dfe9cbf2e46844e03b95a
Snow7yt <112722151+Snow7yt@users.noreply.github.com>
2022-09-27 15:29:25 +0200

    Started the making, will continue in a tad

---
+++ main.cpp
@@ -0,0 +1,7 @@
+/*
+basic guide to chams
+
+this doesnt show everything perfectly that you will need, as I said you need a entity loop and process event function pre-made and working and 
+you can just insert what I show
+*/
+
